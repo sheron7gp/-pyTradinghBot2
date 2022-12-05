7230a36895fa3e1142533ea06dbe06366b2f939b
Your Name <you@example.com>
2022-12-05 04:42:58 -0700

    1.52

--- trdScript_s2.py
+++ trdScript_s2.py
@@ -5,43 +5,43 @@
 import ccxt
 import numpy as np
 from talib import MACD
-# create an instance of the Binance exchange
-binance = ccxt.binance()
-# authenticate with Binance using your API key and secret
-binance.apiKey = "YOUR_API_KEY"
-binance.secret = "YOUR_SECRET"
+# create an instance of the gate exchange
+gate = ccxt.gate()
+# authenticate with gate using your API key and secret
+gate.apiKey = "YOUR_API_KEY"
+gate.secret = "YOUR_SECRET"
 # define the target profit and maximum loss levels for take profit and
 stop-loss orders
 target_profit = 0.05
 max_loss = 0.01
 # fetch the latest price data for the BTC/USDT trading pair
-data = binance.fetch_ticker('BTC/USDT")
+data = gate.fetch_ticker('RIN/USDT")
 # calculate the MACD indicator using the price data
 macd, signal, histogram = MACD(np.array(data['close']), fastperiod=12,
                                slowperiod=26, signalperiod=3)
 # use the MACD signals to make trading decisions
 if macd[-1] > signal[-1] and histogram[-1] > 0:
     # MACD indicates an uptrend, so buy BTC
-order = binance.create_order('BTC/USDT', 'futures', 'buy', 1, 18_000)
+order = gate.create_order('RIN/USDT', 'futures', 'buy', 1, 18_000)
 # set a take profit order to sell BTC when it reaches the target
 profit level
-binance. create_order('8TC/USDT', 'futures', 'sell', 1, 10_000 * (1 +
+gate. create_order('RIN/USDT', 'futures', 'sell', 1, 10_000 * (1 +
                                                                   target_profit), {'orderType': 'TAKE_PROFIT'})
 # set a stop-loss order to sell BTC when it reaches the maximum loss
 level
-binance.create_order('BTC/USDT', 'futures', 'sell', 1, 10_808 * (1 -
+gate.create_order('RIN/USDT', 'futures', 'sell', 1, 10_808 * (1 -
                                                                  max_loss), {'orderType': 'STOP_LOSS'})
 elif macd[-1] < signal[-1] and histogram[-1] < 8:
     # MACD indicates a downtrend, so sell BTC
-order = binance.create_order('BTC/USDT', 'futures', 'sell', 1,
+order = gate.create_order('BTC/USDT', 'futures', 'sell', 1,
                              10_000
                              # set a take profit order to buy BTC when it reaches the target
                              profit level
-                             binance.create_order("BTC/USDT", "futures", 'buy", 1, 10_000 * (1 -
+                             gate.create_order("BTC/USDT", "futures", 'buy", 1, 10_000 * (1 -
                                                                                              target_profit), {'orderType': 'TAKE_PROFIT'})
                              # set a stop-loss order to buy BTC when it reaches the maximum loss
                              level
-                             binance.create_order("BTC/USDT", "futures', 'buy", 1, 10_000 * (1 +
+                             gate.create_order("BTC/USDT", "futures', 'buy", 1, 10_000 * (1 +
                                                                                              max_loss), {'orderType': 'STOP_LOSS'})
 
 
